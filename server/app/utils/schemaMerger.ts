adf4132c772694c7a73ea8e2468fed8673231031
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-03-30 01:15:18 +0100

    :tada: client + server + monorepo initialized

---
+++ server/app/utils/schemaMerger.ts
@@ -0,0 +1,25 @@
+import { readFileSync, writeFileSync } from 'fs';
+import * as path from 'path';
+import { EOL } from 'os';
+
+const dirname = path.dirname(__filename);
+const serverPath = path.dirname(`${dirname}/../../..`);
+
+const schemaNames = [
+  'user',
+];
+
+const generalSchema = schemaNames
+  .map((schemaName) => {
+    const filePath = path.join(dirname, `../schemas/${schemaName}.gql`);
+    return readFileSync(filePath, 'utf-8');
+  })
+  .join(EOL);
+
+const serverDistPath = path.join(serverPath, '/../comon/schemas.gql');
+writeFileSync(serverDistPath, generalSchema, 'utf-8');
+
+// eslint-disable-next-line no-console
+console.log(`Merged schema saved to: ${serverDistPath}`);
+
+export default serverDistPath;
