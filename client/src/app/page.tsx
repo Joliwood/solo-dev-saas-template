8a4085aa1a3d97793b7362f72e56611ab1f76cec
Guillaume Jolibois <jolibois.contact@gmail.com>
2024-04-13 14:00:39 +0200

    :hammer: :tada: client repaired + shadnc ui installed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-// import { Button } from '#components/ui/button';
-// import { SearchInput } from '@/components/SearchInput';
 
-const Home = () => {
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
   return (
-    <main className="h-screen flex flex-col gap-2 justify-center items-center">
-
-      {/* <form role="search">
-        <SearchInput />
-      </form>
-
-      <Button>Default</Button>
-      <Button size="sm">small</Button>
-      <Button size="lg">large</Button>
-      <Button size="icon">i</Button>
-      <Button variant="destructive">Cancel</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="secondary">2nd</Button>
-      <Button variant="ghost" size="icon" className="rounded-full">🚀</Button> */}
-      <span>azpo</span>
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Click me</Button>
     </main>
   );
-};
-
-export default Home;
+}
