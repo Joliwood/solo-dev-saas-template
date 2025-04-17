c423c2c36e7c6335c949b11e20bd8d924251d073
Guillaume Jolibois <jolibois.contact@gmail.com>
2025-04-17 19:44:15 +0200

    Clean stable but bugged state for tailwind

--- projects/client/src/app/page.tsx
+++ projects/client/src/app/page.tsx
@@ -1,7 +1,6 @@
-import { Button } from 'components/ui/button';
 import React from 'react';
 
-// import { Badge, Button } from '#ui-client';
+import { Button } from '#ui-client';
 
 const Home = () => {
   return (
